709763250e1e6dd58aa7f5915f565e0edb1b7537
MRLia <charmed.lia@gmail.com>
2013-08-12 23:13:28 +0200

    Added Enemy, GameState

--- Strokes.py
+++ Strokes.py
@@ -2,8 +2,58 @@ import Leap, sys, pygame, math
 from Leap import SwipeGesture
 
 
-class SampleListener(Leap.Listener):
+class Stroke:
+	positions = []
+	strokeID = None
+	def __init__(self,id,pos):
+		self.strokeID = id
+		self.positions = []
+		self.positions.append(pos)
+
+	def addPos(self, pos):
+		self.positions.append(pos)
+
+	def draw(self):
+		prevPos = self.positions[0]
+		for pos in self.positions:
+			pygame.draw.line(screen, (0,0,0), prevPos, pos, 3)
+			prevPos = pos
+
+class Enemy:
+	strokes = []
+	name = ""
+	curStroke = 0
+	def __init__(self,strokeList,name):
+		self.strokes = strokeList
+		self.name = name
+
+	def attack(self,stroke):
+		if isSimilar(self.strokes[self.curStroke],stroke):
+			self.curStroke = self.curStroke + 1
+
+	def isDefeated(self):
+		return self.curStroke == len(self.strokes)
+
+def isSimilar(firstStroke,secondStroke):
+	return True
+
+def getEnemy():
+	strokes = [Stroke(1,(300,400))]
+	
+	return Enemy(strokes,"boca")
+
+class GameState:
+	enemy = getEnemy()
+
+	def attack(self,stroke):
+		self.enemy.attack(stroke)
+		if self.enemy.isDefeated():
+			print "Enemy {} defeated!!!!".format(self.enemy.name)
+			self.enemy = getEnemy()
+
+class GameListener(Leap.Listener):
 	curStroke = None
+	gameState = GameState()
 	def on_init(self, controller):
 		print "Initialized"
 
@@ -26,34 +76,21 @@ class SampleListener(Leap.Listener):
 			print swipe.position
 			positionList = swipe.position.to_tuple()
 			curPos = [int(positionList[0]+500),int(math.fabs(positionList[1]-500))]
-			if self.curStroke != None and self.curStroke.strokeID == swipe.id:
-				self.curStroke.addPos(curPos)
-			else:
+			if swipe.state == Leap.Gesture.STATE_START:
 				self.curStroke = Stroke(swipe.id,curPos)
+			elif swipe.state == Leap.Gesture.STATE_UPDATE:
+				self.curStroke.addPos(curPos)
+			elif swipe.state == Leap.Gesture.STATE_STOP:
+				self.gameState.attack(self.curStroke)
+
 			self.curStroke.draw()
 			pygame.display.flip()
 
-class Stroke:
-	positions = []
-	strokeID = None
-	def __init__(self,id,pos):
-		self.strokeID = id
-		self.positions = []
-		self.positions.append(pos)
-
-	def addPos(self, pos):
-		self.positions.append(pos)
-
-	def draw(self):
-		prevPos = self.positions[0]
-		for pos in self.positions:
-			pygame.draw.line(screen, (0,0,0), prevPos, pos, 3)
-			prevPos = pos
 
 screen = pygame.display.set_mode([900,600])
 def main():
 	# Create a sample listener and controller
-	listener = SampleListener()
+	listener = GameListener()
 	controller = Leap.Controller()
 	pygame.init()
 	screen.fill((0,255,255))
