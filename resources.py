2bfe976c2db6c5449f3283b8de54a794435d3ba3
MRLia <charmed.lia@gmail.com>
2013-08-15 01:44:48 +0200

    Updated to manage the update of the screen

--- resources.py
+++ resources.py
@@ -5,6 +5,10 @@ from pygame.locals import *
 
 SCREENRECT = Rect(0, 0, 900, 600)
 screen = pygame.display.set_mode(SCREENRECT.size)
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+bgdImage  = load_image('data','background.jpg')
+background.blit(bgdImage, (0, 0))
 
 def init_window():
     # screen mode
@@ -13,17 +17,19 @@ def init_window():
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestColorDepth)
 
     # Create the visualitation window
-    screen.fill((0,255,255))
-    pygame.display.flip()
     pygame.display.set_icon(load_image('data','icon.gif'))
     pygame.display.set_caption('Kanji Strokes')
 
-def init_background():    
+def init_background():
     # Create the background
-    bgdImage  = load_image('background.bmp')
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.blit(bgdImage, (0, 0))
-
     screen.blit(background, (0,0))
     pygame.display.flip()
+
+def get_screen():
+    return screen
+
+def get_background():
+    return background
+
+def update_screen():
+    screen.blit(background, (0,0))
