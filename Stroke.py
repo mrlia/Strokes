b984711e287e9dca4ff7b95b7527db0c04396e0c
MRLia <charmed.lia@gmail.com>
2013-08-16 21:18:55 +0200

    Added stroke comparation in isSimilar

--- Stroke.py
+++ Stroke.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, math
 
 class Stroke:
 	"""This class represents a player stroke."""
@@ -7,7 +7,8 @@ class Stroke:
 	def __init__(self,id,pos):
 		self.strokeID = id
 		self.positions = []
-		self.positions.append(pos)
+		if pos != None :
+			self.positions.append(pos)
 
 	def addPos(self, pos):
 		self.positions.append(pos)
@@ -18,6 +19,14 @@ class Stroke:
 			pygame.draw.line(surface, (0,0,0), prevPos, pos, 3)
 			prevPos = pos
 
+def getAngle(stroke):
+	return math.atan2(stroke.positions[-1][1] - stroke.positions[0][1], stroke.positions[-1][0] - stroke.positions[0][0])
+
 def isSimilar(firstStroke,secondStroke):
-	return True
+	x = getAngle(firstStroke)
+	y = getAngle(secondStroke)
+	result = abs(x-y)
+	print "result:"
+	print result
+	return result < 0.3
 
